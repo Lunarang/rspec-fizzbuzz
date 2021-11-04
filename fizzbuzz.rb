20ed3a368b6a0f79f75e8d3301755d32473d7d31
Lunarang <ecmettille@gmail.com>
2021-11-04 08:22:49 -0700

    commit tree 705341e40183ce27424f43fe4e4059a614f0a1b6

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+    divBy3 = ( number%3 == 0 )
+    divBy5 = ( number%5 == 0 )
+    
+    if divBy3 && divBy5
+        puts "FizzBuzz"
+        "FizzBuzz"
+    elsif divBy5
+        puts "Buzz"
+        "Buzz"
+    elsif divBy3
+        puts "Fizz"
+        "Fizz"
+    else
+        puts "No FizzBuzz"
+    end
+end
